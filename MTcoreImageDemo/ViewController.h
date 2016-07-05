38373cf40493fddec5cdc634c5736a8bced62ff1
meitu <zj-db0147@meitudemacbook-pro.local>
2016-07-05 09:26:11 +0800

    Initial Commit

---
+++ MTcoreImageDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MTcoreImageDemo
+//
+//  Created by meitu on 16/7/5.
+//  Copyright © 2016年 meitu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
